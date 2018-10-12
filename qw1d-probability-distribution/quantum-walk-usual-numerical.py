5b5ad3fe53d73b2bd5f9e4602400ad229b9d2ce0
PiresMA <pires.ma.fisica@gmail.com>
2018-10-12 00:59:22 -0300

    Add files via upload

---
+++ qw1d-probability-distribution/quantum-walk-usual-numerical.py
@@ -0,0 +1,149 @@
+# Quantum Walk:  numerical approach based on table 1 of [1]
+#
+# References:
+# [1] Kendon, Vivien M. "A random walk approach to quantum algorithms." Philos. Trans. Royal Soc. A. 364.1849 (2006): 3407-3422.
+# [2] J.Dingerink: Quantum walks of an atom in an optical lattice, 2016 https://esc.fnwi.uva.nl/thesis/centraal/files/f1472237352.pdf
+# [3] Portugal, Renato. Quantum walks and search algorithms. Springer Science & Business Media, 2013.
+
+import math as mt
+import numpy   as   np
+from matplotlib import pylab as plt
+
+nsteps = 100
+
+
+
+# Main part: see line 110
+
+
+
+# One Step:  1) Hadamard coin is flipped; 2) shift operator works on the state
+def psiStep( psiInput ):
+ # This part is based on appendix B of [2]
+ psiOutput  =  [ ]
+ for elem in psiInput:
+   if elem[2]   ==  0:  # if the Hadamard coin state is \0>
+    psiOutput.append([ elem[0]/mt.sqrt(2), elem[1]-1, 0]) # shift operator: S-
+    psiOutput.append([ elem[0]/mt.sqrt(2), elem[1]+1, 1]) # shift operator: S+
+   elif elem[2] == 1:   # if the Hadamard coin state is \1>
+    psiOutput.append([  elem[0]/mt.sqrt(2), elem[1]-1, 0]) # shift operator: S-
+    psiOutput.append([ -elem[0]/mt.sqrt(2), elem[1]+1, 1]) # shift operator: S+   
+ return(psiOutput)
+
+
+# To remove duplicate elements 
+def Remove(duplicate): 
+    final_list = [] 
+    for num in duplicate: 
+        if num not in final_list: 
+            final_list.append(num) 
+    return final_list 
+
+
+# Putting terms together: states with the same Position and Coin state
+def psiGathering( psiInput ):
+ psiPC = []   # PC = Position and Coin state
+ for elem in psiInput:
+  psiPC.append( [elem[1],elem[2]] )
+  
+ psiPC2 = Remove(psiPC) #Removing duplicate elements of the full psi
+
+ vecamplit = []  # For amplitude
+ for elem2 in psiPC2:
+  amplit = 0
+  label  = -1
+  for elem in psiPC:
+   label = label + 1
+   if elem2 == elem:
+    amplit = amplit + psiInput[label][0] # Sum probabiliy amplitudes 
+  vecamplit.append(amplit)
+
+ psiOutput = []
+ for i in range(len(psiPC2)):
+  psiOutput.append( [ vecamplit[i], psiPC2[i][0], psiPC2[i][1] ] )
+
+ return psiOutput
+
+
+
+# Putting terms together to compute the probability distribution
+def probabilityDistribution( psiInput ):
+ pisP = []
+ for elem in psi:
+  pisP.append( elem[1] )
+  
+ #Removing duplicate elements of the full psi
+ pisP2 = []
+ for i in pisP:
+  if i not in pisP2:
+   pisP2.append(i) 
+
+ vecamplit = []   # For full amplitude
+ for elem2 in pisP2:
+  amplit = 0
+  label  = -1
+  for elem in pisP:
+   label = label + 1
+   if elem2 == elem:
+    amplit = amplit + abs( psiInput[label][0] )**2 # Sum probabiliy amplitudes 
+  vecamplit.append(amplit)
+  
+ # Get the occupancy probability distribution
+ posProb = []
+ for i in range(len(pisP2)):  
+  posProb.append( [ pisP2[i], vecamplit[i], ] ) # output: [position,probability]
+
+ return posProb 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####  MAIN PART 
+
+#print("Notation:  psi = [pre-factor(coefficient), position(walker), spin(coin)]\n") 
+
+
+#####  time  = 0
+#psi0asymmetric = [[1, 0, 1]]    # Ao=1  Bo=0
+psi0symmetric  = [ [1/mt.sqrt(2), 0,1], [ 1j/mt.sqrt(2), 0, 0] ]
+
+psi = psi0symmetric 
+
+##### dynamics 
+for t in  np.arange(1,nsteps,1):   
+ psi = psiStep( psi )
+ psi = psiGathering( psi )
+
+
+# Get the occupancy probability distribution
+vecamplitNumer = probabilityDistribution(psi)
+
+
+# only sites with  non-zero occupancy probability
+vecpos  = []
+vecprob = []
+for i in np.arange(0,len(vecamplitNumer),1):  
+ if vecamplitNumer[i][1]>0:
+  vecpos.append(  vecamplitNumer[i][0] )
+  vecprob.append( vecamplitNumer[i][1] )
+
+
+
+plt.plot(vecpos,vecprob, color='red', linestyle='dashed', marker='o', markerfacecolor='red', markersize=12)
+plt.xlim(-nsteps,nsteps)
+plt.xlabel("Position")
+plt.ylabel("Probability")
+plt.title(" Numerical approach (table 1 of V.M.Kendon 2006) \nQuantum Walk for {0} steps".format(nsteps))
+plt.savefig("probability-distribution-numerical.png")
+
+
+
---
+++ qw1d-probability-distribution/quantum-walk-usual-semi-analytical.py
@@ -0,0 +1,80 @@
+# Quantum Walk: semi-analytical approach based on the recursive method described in page 27 of [3]
+#
+# References:
+# [1] Kendon, Vivien M. "A random walk approach to quantum algorithms." Philos. Trans. Royal Soc. A. 364.1849 (2006): 3407-3422.
+# [2] J.Dingerink: Quantum walks of an atom in an optical lattice, 2016 https://esc.fnwi.uva.nl/thesis/centraal/files/f1472237352.pdf
+# [3] Portugal, Renato. Quantum walks and search algorithms. Springer Science & Business Media, 2013.
+
+
+import cmath as cmt
+import math as mt
+import numpy as np
+from matplotlib import pylab as plt
+
+nsteps = 100         # number of steps
+
+
+
+# Putting terms together to compute the probability distribution
+def recursive_A_B(vecA, vecB):
+ vecA2   = []
+ vecB2   = []
+
+ # Left boundary
+ vecA2.append( 0 ) 
+ vecB2.append( 0 ) 
+
+ # Other positions
+ for i in  np.arange(1,len(xpos)-1,1):    
+   vecA2.append( (vecA[i-1] + vecB[i-1])/mt.sqrt(2) ) 
+   vecB2.append( (vecA[i+1] - vecB[i+1])/mt.sqrt(2) ) 
+
+ # Right boundary
+ vecA2.append( 0 ) 
+ vecB2.append( 0 ) 
+
+ return vecA2,vecB2
+ 
+ 
+ 
+ 
+
+npos   = 2*nsteps+1  # number of positions
+xc     = nsteps      # central position = nsteps   #Index Start at 0
+
+xpos = np.arange(-nsteps, nsteps+1, 1)   
+
+
+#####  time  = 0
+vecB     =  np.zeros(npos, dtype=np.complex_)    
+vecA     = np.zeros(npos)
+vecA[xc] =  1/mt.sqrt(2)
+vecB[xc] = 1j/cmt.sqrt(2)   
+
+
+##### Dynamics
+for t in  np.arange(1,nsteps,1):
+ vecA,vecB = recursive_A_B(vecA, vecB)
+
+#print( np.around(np.array( vecA ),2) )
+#print( np.around(np.array( vecB ),2) )
+
+
+# Only sites with non-zero occupancy probability
+vecpos  = [ ]
+vecprob = [ ]
+for i in  np.arange(0,len(xpos),1):
+ prob =  abs(vecA[i])**2 +  abs(vecB[i])**2  
+ if prob>0:
+  vecpos.append(  xpos[i] )
+  vecprob.append(  prob )
+   
+
+plt.plot(vecpos,vecprob, color='blue', linestyle='dashed', marker='o', markerfacecolor='blue', markersize=12)
+plt.xlim(-nsteps,nsteps)
+plt.xlabel("Position")
+plt.ylabel("Probability")
+plt.title(" Semi-analytical approach (pag. 27 of R.Portugal 2013) \nQuantum Walk for {0} steps".format(nsteps))
+plt.savefig("probability-distribution-semi-analytical.png")
+
+
